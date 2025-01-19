0d28bb571408284733bcf0fb1f7b22f57e3ea59d
Geethika Radhakrishnan <geethikaradhakrishnan@gmail.com>
2025-01-19 15:45:54 +0530

    implemented a sample fetch_api function

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import argparse 
 from argparse import Namespace
+import requests
+import json
 
 def parser_func() -> Namespace:
     parser = argparse.ArgumentParser(description = "Fetch the GitHub username from CLI")
     parser.add_argument('user_name', type = str, help = "The GitHub username to fetch user activity")
 
     args : Namespace = parser.parse_args()
-    return args
+    return args.user_name
+
+def fetch_api(username : str):
+    url = f'https://api.github.com/users/{username}/events'
+    response = requests.get(url)
+    events = response.json()
+    return events
 
 def main() -> None:
-    args : Namespace = parser_func()
-    
+    username : str = parser_func()
+    event = fetch_api(username)
+    print(event)
+
 if __name__ == '__main__':
     main()
